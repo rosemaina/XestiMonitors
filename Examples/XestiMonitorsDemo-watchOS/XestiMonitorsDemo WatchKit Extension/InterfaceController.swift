06ebd413ffdc329c8515875eea262598407a53cc
J. G. Pusey <ebardx@gmail.com>
2018-01-12 17:09:10 -0500

    Skeletal app

---
+++ Examples/XestiMonitorsDemo-watchOS/XestiMonitorsDemo WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  XestiMonitorsDemo WatchKit Extension
+//
+//  Created by John Pusey on 1/12/18.
+//  Copyright © 2018 Xesticode. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
