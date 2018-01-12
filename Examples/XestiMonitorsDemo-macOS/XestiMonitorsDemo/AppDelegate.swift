9902fb7bc86f8e798828f8305c69caf6f2a3c496
J. G. Pusey <ebardx@gmail.com>
2018-01-12 17:31:55 -0500

    Skeletal app

---
+++ Examples/XestiMonitorsDemo-macOS/XestiMonitorsDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XestiMonitorsDemo
+//
+//  Created by John Pusey on 1/12/18.
+//  Copyright © 2018 Xesticode. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Examples/XestiMonitorsDemo-macOS/XestiMonitorsDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XestiMonitorsDemo
+//
+//  Created by John Pusey on 1/12/18.
+//  Copyright © 2018 Xesticode. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
